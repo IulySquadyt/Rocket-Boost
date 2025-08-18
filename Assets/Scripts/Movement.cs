52e91d9a0306557a739f011e7fd59abe13f67db6
Popa Iulian-Marian <iulianpopa167@gmail.com>
2025-08-18 12:31:27 +0300

    Using AddRelativeForce

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,25 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustForce = 10f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
     }
-    void Update()
+    void FixedUpdate()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Thrust button is pressed");
+            Debug.Log("Thrusting!");
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
         }
     }
 }
